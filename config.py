4659d123def930d6149435e8948134eae3ec0f3f
santiagolavista92-cyber <santiago.lavista92@gmail.com>
2026-02-24 20:22:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ import os
 
 # Токен берётся из переменной окружения (Railway)
 # или напрямую если запускаешь локально
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ТВОЙ_ТОКЕН_ЗДЕСЬ")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Твой Telegram ID (получи у @userinfobot)
-ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))
+ADMIN_ID = int(os.getenv("ADMIN_ID")
 
 # Ссылка на тебя для связи
 ADMIN_USERNAME = "@Saintbeat_bot"
